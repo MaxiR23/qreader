c841dfb8e4a5adadc8343fafd9d87036b7abeae6
Maxi Rebolo <maximilianorebolo@gmail.com>
2020-02-27 00:11:43 -0300

    Primer commit

---
+++ lib/src/pages/direction_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DirecctionPage extends StatelessWidget {
+  const DirecctionPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
